b384fdf85ef7392ba6ebd663fec9b09097b34673
angeelmdev <anmorenomunoz@gmail.com>
2025-08-19 11:07:17 +0200

    feat: improves the aesthetics of the interface

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: true,
+  content: [
+    "./templates/**/*.{html,twig}",
+    "./assets/**/*.{js,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
